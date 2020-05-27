eac4f3feb452dff548aa32da69e5b2e2226dbd13
MartinFalco <34011391+Sokoma@users.noreply.github.com>
2020-05-27 23:14:46 +0200

    Setup for Heroku

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
+const port = process.env.PORT || 3000;
+
 const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
@@ -10,6 +12,6 @@ app.get('/help', (request, response) => {
 	response.send('Get help!');
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
 	console.log('Server is up and running!');
 });
